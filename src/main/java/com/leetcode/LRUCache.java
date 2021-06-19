fb7c23aacfd3d3d3e6a5dab6a88eafe85f48fa5d
houguokun <hgk324@163.com>
2021-06-19 22:27:43 +0800

    LRUCache update

--- src/main/java/com/leetcode/LRUCache.java
+++ src/main/java/com/leetcode/LRUCache.java
@@ -2,15 +2,26 @@ package com.leetcode;
 
 import java.util.HashMap;
 
-public class LRUCache {
+class Node {
+    int key;
+    int value;
+    Node pre;
+    Node next;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class LRUCache {
     private HashMap<Integer, Node> map;
-    private int capicity, count;
+    private int capacity, count;
     private Node head, tail;
 
-    LRUCache(int cap) {
-        this.capicity = cap;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
         map = new HashMap<>();
-
         head = new Node(0, 0);
         tail = new Node(0, 0);
         head.next = tail;
@@ -20,64 +31,90 @@ public class LRUCache {
         count = 0;
     }
 
+    public static void main(String[] args) {
+        System.out.println("Going to test the LRU "
+                + " Cache Implementation");
+        LRUCache cache = new LRUCache(2);
+
+        // it will store a key (1) with value
+        // 10 in the cache.
+        cache.set(1, 10);
+
+        // it will store a key (1) with value 10 in the
+        // cache.
+        cache.set(2, 20);
+        System.out.println("Value for the key: 1 is "
+                + cache.get(1)); // returns 10
+
+        // evicts key 2 and store a key (3) with
+        // value 30 in the cache.
+        cache.set(3, 30);
+
+        System.out.println(
+                "Value for the key: 2 is "
+                        + cache.get(2)); // returns -1 (not found)
+
+        // evicts key 1 and store a key (4) with
+        // value 40 in the cache.
+        cache.set(4, 40);
+        System.out.println(
+                "Value for the key: 1 is "
+                        + cache.get(1)); // returns -1 (not found)
+        System.out.println("Value for the key: 3 is "
+                + cache.get(3)); // returns 30
+        System.out.println("Value for the key: 4 is "
+                + cache.get(4)); // return 40
+    }
+
+    public void deleteNode(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    public void addToHead(Node node) {
+        node.next = head.next;
+        node.next.pre = node;
+        node.pre = head;
+        head.next = node;
+    }
+
     // This method works in O(1)
     public int get(int key) {
-        // your code here
         if (map.get(key) != null) {
             Node node = map.get(key);
+            int result = node.value;
             deleteNode(node);
-            addNodeHead(node);
-            return node.value;
-        } else {
-            return -1;
+            addToHead(node);
+            System.out.println("Got the value : " + result
+                    + " for the key: " + key);
+            return result;
         }
-    }
-
-    private void addNodeHead(Node node) {
-        Node node2 = head.next;
-        head.next = node;
-        node.pre = head;
-        node.next = node2;
-        node2.pre = node;
-    }
-
-    private void deleteNode(Node node) {
-        Node pre = node.pre;
-        Node next = node.next;
-        pre.next = next;
-        next.pre = pre;
+        System.out.println("Did not get any value"
+                + " for the key: " + key);
+        return -1;
     }
 
     // This method works in O(1)
-    public void get(int key, int value) {
+    public void set(int key, int value) {
+        System.out.println("Going to set the (key, "
+                + "value) : (" + key + ", "
+                + value + ")");
         if (map.get(key) != null) {
             Node node = map.get(key);
             node.value = value;
             deleteNode(node);
-            addNodeHead(node);
+            addToHead(node);
         } else {
             Node node = new Node(key, value);
             map.put(key, node);
-            if (count < capicity) {
+            if (count < capacity) {
                 count++;
+                addToHead(node);
             } else {
                 map.remove(tail.pre.key);
                 deleteNode(tail.pre);
+                addToHead(node);
             }
-            addNodeHead(node);
-        }
-    }
-
-
-    class Node {
-        int key;
-        int value;
-        Node pre;
-        Node next;
-
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
         }
     }
 }
